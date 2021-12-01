456f1448e4904608340d542e59bf1db099b50643
Nick <nickdloucks@outlook.com>
2021-11-30 20:46:21 -0800

    dummy code for setup

---
+++ index.ts
@@ -0,0 +1,4 @@
+// index.ts
+import { randomNumber } from "libs/number";
+
+console.log("Random number (1 - 100): ", randomNumber());
---
+++ libs/number.ts
@@ -0,0 +1,5 @@
+// libs/number.ts
+export function randomNumber(): number {
+    // random from 1 to 100
+    return Math.floor(Math.random() * 99) + 1;
+  }
