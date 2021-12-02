b8f5fc05ddf2ce36c058ab7dfe4c2abfb350647c
Nick <nickdloucks@outlook.com>
2021-12-02 12:33:44 -0800

    outline for routes and import dependencies

---
+++ src/routes/games.router.ts
@@ -0,0 +1,16 @@
+// External Dependencies
+
+import express, { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import { collections } from "../services/database.service";
+import Game from "../models/game";
+
+// Global Config
+
+// GET
+
+// POST
+
+// PUT
+
+// DELETE
