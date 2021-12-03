b6af4cb7313aa46410bede35737041f98181cd43
Nick <nickdloucks@outlook.com>
2021-12-02 19:13:33 -0800

    POST and second GET route

--- src/routes/games.router.ts
+++ src/routes/games.router.ts
@@ -28,7 +28,37 @@ gamesRouter.get("/", async (_req: Request, res: Response) => {
     }
 });
 
+gamesRouter.get("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        
+        const query = { _id: new ObjectId(id) };
+        const game = (await collections.games.findOne(query)) as Game;
+
+        if (game) {
+            res.status(200).send(game);
+        }
+    } catch (error) {
+        res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
+    }
+});
+
 // POST
+
+gamesRouter.post("/", async (req: Request, res: Response) => {
+    try {
+        const newGame = req.body as Game;
+        const result = await collections.games.insertOne(newGame);
+
+        result
+            ? res.status(201).send(`Successfully created a new game with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new game.");
+    } catch (error) {
+        console.error(error);
+        res.status(400).send(error.message);
+    }
+});
 
 // PUT
 
