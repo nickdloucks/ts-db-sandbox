d9bbb6f6f68cd36deefddee36cc9dc0fd67da442
Nick <nickdloucks@outlook.com>
2021-12-03 11:29:32 -0800

    move index into src folder, connect2DB

--- index.ts
+++
@@ -1,6 +0,0 @@
-// index.ts
-import express from "express";
-import { connectToDatabase } from "./services/database.service"
-import { gamesRouter } from "./routes/games.router";
-
-console.log("Random number (1 - 100): ", randomNumber());
