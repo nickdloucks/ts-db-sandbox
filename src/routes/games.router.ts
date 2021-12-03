dac5e2af3b02934cb3f0c18f62667cc2dfacf398
Nick <nickdloucks@outlook.com>
2021-12-02 19:15:17 -0800

    DELETE

--- src/routes/games.router.ts
+++ src/routes/games.router.ts
@@ -81,3 +81,23 @@ gamesRouter.put("/:id", async (req: Request, res: Response) => {
 });
 
 // DELETE
+
+gamesRouter.delete("/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try {
+        const query = { _id: new ObjectId(id) };
+        const result = await collections.games.deleteOne(query);
+
+        if (result && result.deletedCount) {
+            res.status(202).send(`Successfully removed game with id ${id}`);
+        } else if (!result) {
+            res.status(400).send(`Failed to remove game with id ${id}`);
+        } else if (!result.deletedCount) {
+            res.status(404).send(`Game with id ${id} does not exist`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).send(error.message);
+    }
+});
