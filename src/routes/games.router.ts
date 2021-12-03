42306ff22815f7aea33870b25140864874ab6e62
Nick <nickdloucks@outlook.com>
2021-12-02 19:07:01 -0800

    add GET route

--- src/routes/games.router.ts
+++ src/routes/games.router.ts
@@ -11,7 +11,22 @@ export const gamesRouter = express.Router();
 
 gamesRouter.use(express.json());
 
+        /**
+         * We have to tell our router to use the json parser middleware built into Express, 
+         * which is why we call use(express.json());.
+         */
+
 // GET
+
+gamesRouter.get("/", async (_req: Request, res: Response) => {
+    try {
+       const games = (await collections.games.find({}).toArray()) as Game[];
+
+        res.status(200).send(games);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
 // POST
 
